df677c0efca770d30de5cb89ab60e7828adde24e
cieslikprzemyslaw <cprzemek91@hotmail.com>
2020-09-13 13:30:38 +0100

    changed path to characters

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import '../Css/App.css';
 import Characters from './Characters/Characters';
+import CharacterPage from './Characters/CharacterPage'
+import ErrorPage from '../Components/Error/ErrorPage';
 
 function App() {
   return (
     <Router>
       <div className="App">
-          <Characters/>
+        <section>
+          <Switch>
+            <Route path="/" exact component={Characters} />
+            <Route path="/character/:id" component={CharacterPage} />
+            <Route component={ErrorPage} />
+          </Switch>
+        </section>
       </div>
     </Router>
   );

c1c6c7ea5de42667d4a31e5f997d249fb458d5f9
cieslikprzemyslaw <cprzemek91@hotmail.com>
2020-09-25 22:13:53 +0100

    gh-pages

---
+++ root/precache-manifest.e255c35a557aafb88f2568c33e94cc1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deafb815d2e5736431b220db194b12ba",
+    "url": "/LegueOfLegends_Characters/index.html"
+  },
+  {
+    "revision": "3df25aa4063b1ec6953b",
+    "url": "/LegueOfLegends_Characters/static/css/main.7ce30feb.chunk.css"
+  },
+  {
+    "revision": "2cdca2f313d1bef820ad",
+    "url": "/LegueOfLegends_Characters/static/js/2.78f38547.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/LegueOfLegends_Characters/static/js/2.78f38547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df25aa4063b1ec6953b",
+    "url": "/LegueOfLegends_Characters/static/js/main.a02bfd34.chunk.js"
+  },
+  {
+    "revision": "6f74d67740934fdd4653",
+    "url": "/LegueOfLegends_Characters/static/js/runtime-main.8a51686a.js"
+  }
+]);
